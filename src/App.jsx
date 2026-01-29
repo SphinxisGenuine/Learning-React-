b8c950a266859ecb149595b0b982ce40912524d7
Sphinx <sphinxisgenuine@gmail.com>
2026-01-29 21:02:19 +0530

    first implementation of UseDeb FP

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+  function useDebounce(func,delay) {
+  let id=useRef(null);
+  const fn =()=>{
+    clearTimeout(id.current)
+id.current=setTimeout(func,delay);
+  }
+  return fn;
+}
+
 
 function App() {
-  return <>
-  </>
+  
+const [saving,setsaving]=useState(false)
+function AutoSave(){
+setsaving(true);
+fetch("ebtbrtteeebtbe").finally(()=>(setsaving(false)))
 }
+
+
+
+const debouncefnc=useDebounce(AutoSave,2000);
+
+
+  return <>
+
+<textarea onChange={debouncefnc} name ="inptBox" rows={3} cols={50}> </textarea>
+{saving&&<p>Saving..</p>}   
+
+</>}
 
 export default App;
 
